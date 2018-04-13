fed4952d2ba99efc4fc776cce58d002fce11adc7
r-tsuboi <r-tsuboi@allmedia.co.jp>
2018-04-13 02:31:01 +0000

    レッスン10調整前

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,7 @@ class PostsController < ApplicationController
   def show
     @post = Post.find_by(id: params[:id])
     @user = @post.user
+    @likes_count = Like.where(post_id: @post.id).count
   end
 
   def new
@@ -52,11 +53,10 @@ class PostsController < ApplicationController
 
   def ensure_correct_user
     @post = Post.find_by(id: params[:id])
-    if @current_user.id != @post.user_id
+    if @post.user_id != @current_user.id
       flash[:notice] = "権限がありません"
       redirect_to("/posts/index")
     end
   end
-
 
 end
